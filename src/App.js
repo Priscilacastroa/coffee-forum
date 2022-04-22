498cdfbcc965bc390d10c32de8a87c2a7398adeb
Anna Reis <reis.annabeatriz@outlook.com>
2022-04-22 11:32:31 -0300

    commit da anna

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Minha dupla nao me valoriza!</h1>
+    </div>
+  );
 }
 
 export default App;
